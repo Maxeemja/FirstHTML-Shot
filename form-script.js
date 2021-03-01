448f726d4859ce9c8433155d83934e1c952a6bd9
Maxeemja <71792881+Maxeemja@users.noreply.github.com>
2021-03-01 19:11:05 +0200

    Add files via upload

---
+++ form-script.js
@@ -0,0 +1,10 @@
+function check_form(f){
+    var name = f.name.value;
+    var pass = f.pass.value;
+    var repass = f.repass.value;
+    var error = ""
+    if(name.length < 2 ){
+      alert("Введите правильное имя!")
+    }
+
+}
